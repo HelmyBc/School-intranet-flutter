2a326435b1439235771d0caa84aa240a89041764
HelmyBc <helmibouchiha@gmail.com>
2022-05-13 00:37:52 +0100

    version 5.4 create postScreen done with dynamic UI

--- lib/views/home screen/create_post_screen.dart
+++ lib/views/home screen/create_post_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dotted_border/dotted_border.dart';
 import 'package:enetcom_app/controllers/post_controller.dart';
 import 'package:enetcom_app/controllers/user_controller.dart';
 import 'package:enetcom_app/data/data.dart';
@@ -8,6 +9,7 @@ import 'package:enetcom_app/models/attachment.dart';
 import 'package:enetcom_app/models/post.dart';
 import 'package:enetcom_app/services/http_post_service.dart';
 import 'package:enetcom_app/views/widgets/profile_avatar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
@@ -233,175 +235,173 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
             )
           ],
         ),
-        body: Form(
-          child: ListView(
-            children: [
-              const SizedBox(height: 20.0),
-              Container(
-                margin: const EdgeInsets.all(10.0),
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.white,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(0.0, 2.0),
-                      blurRadius: 6.0,
+        body: ListView(
+          //physics: const ClampingScrollPhysics(),
+          shrinkWrap: true,
+          children: [
+            Form(
+              child: ListView(
+                physics: const ClampingScrollPhysics(),
+                shrinkWrap: true,
+                children: [
+                  const SizedBox(height: 20.0),
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(0.0, 2.0),
+                          blurRadius: 6.0,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Row(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
                             children: [
-                              ProfileAvatar(
-                                  imageUrl: userController
-                                      .currentUser.value.imageUrl!),
-                              const SizedBox(width: 8.0),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "${userController.currentUser.value.firstName} ${userController.currentUser.value.lastName}",
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Row(
+                              Row(
+                                children: [
+                                  ProfileAvatar(
+                                      imageUrl: userController
+                                          .currentUser.value.imageUrl!),
+                                  const SizedBox(width: 8.0),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
                                         Text(
-                                          'Public • ',
-                                          style: TextStyle(
-                                            color: Colors.grey[600],
-                                            fontSize: 12.0,
+                                          "${userController.currentUser.value.firstName} ${userController.currentUser.value.lastName}",
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.w600,
                                           ),
                                         ),
-                                        Icon(
-                                          Icons.public,
-                                          color: Colors.grey[600],
-                                          size: 12.0,
-                                        )
+                                        Row(
+                                          children: [
+                                            Text(
+                                              'Public • ',
+                                              style: TextStyle(
+                                                color: Colors.grey[600],
+                                                fontSize: 12.0,
+                                              ),
+                                            ),
+                                            Icon(
+                                              Icons.public,
+                                              color: Colors.grey[600],
+                                              size: 12.0,
+                                            )
+                                          ],
+                                        ),
                                       ],
                                     ),
-                                  ],
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10.0),
+                              TextFormField(
+                                minLines: 3,
+                                maxLines: 7,
+                                controller: descriptionController,
+                                keyboardType: TextInputType.multiline,
+                                decoration: const InputDecoration(
+                                  hintText: "Hey, what's on your mind?",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20.0)),
+                                  ),
+                                ),
+                              ),
+                              //Text(widget.post.description),
+                              const SizedBox(height: 10.0),
+
+                              Center(
+                                child: FutureBuilder<void>(
+                                  future: retriveLostData(),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<void> snapshot) {
+                                    switch (snapshot.connectionState) {
+                                      case ConnectionState.none:
+                                      case ConnectionState.waiting:
+                                        return const Text('Picked an image');
+                                      case ConnectionState.done:
+                                        return _previewImage();
+                                      default:
+                                        return const Text('Picked an image');
+                                    }
+                                  },
                                 ),
                               ),
                             ],
                           ),
-                          const SizedBox(height: 10.0),
-                          // Padding(
-                          //   padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          //   child: TextFormField(
-                          //     keyboardType: TextInputType.name,
-                          //     controller: usernameController,
-                          //     validator: (value) {
-                          //       if (value == null) {
-                          //         return "Please enter your username";
-                          //       }
-                          //       return null;
-                          //     },
-                          //     decoration: InputDecoration(
-                          //       labelText: 'username',
-                          //       hintText: 'username',
-                          //       border: OutlineInputBorder(
-                          //         borderRadius: BorderRadius.circular(20.0),
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
-                          // Padding(
-                          //   padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          //   child: TextFormField(
-                          //     keyboardType: TextInputType.number,
-                          //     controller: uidController,
-                          //     validator: (value) {
-                          //       if (value == null) {
-                          //         return "Please enter your uid";
-                          //       }
-                          //       return null;
-                          //     },
-                          //     decoration: InputDecoration(
-                          //       labelText: 'uid',
-                          //       hintText: 'uid',
-                          //       border: OutlineInputBorder(
-                          //         borderRadius: BorderRadius.circular(20.0),
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
-                          // Padding(
-                          //   padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          //   child: TextFormField(
-                          //     keyboardType: TextInputType.url,
-                          //     controller: profImageController,
-                          //     validator: (value) {
-                          //       if (value == null) {
-                          //         return "Please enter your profImage";
-                          //       }
-                          //       return null;
-                          //     },
-                          //     decoration: InputDecoration(
-                          //       labelText: 'prof Image',
-                          //       hintText: 'prof Image',
-                          //       border: OutlineInputBorder(
-                          //         borderRadius: BorderRadius.circular(20.0),
-                          //       ),
-                          //     ),
-                          //   ),
-                          // ),
-                          const SizedBox(height: 18.0),
-                          TextFormField(
-                            minLines: 3,
-                            maxLines: 7,
-                            controller: descriptionController,
-                            keyboardType: TextInputType.multiline,
-                            decoration: const InputDecoration(
-                              hintText: "Hey, what's on your mind?",
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20.0)),
-                              ),
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            _imageFile == null
+                ? GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 10.0),
+                      child: DottedBorder(
+                        padding: const EdgeInsets.all(0),
+                        borderType: BorderType.RRect,
+                        radius: const Radius.circular(20),
+                        dashPattern: const [10, 10],
+                        color: Colors.blue.shade800,
+                        strokeWidth: 2,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 1,
+                          padding: const EdgeInsets.all(40.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.lightBlue.withOpacity(0.3),
+                          ),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Icon(
+                                  CupertinoIcons.photo,
+                                  color: Colors.blue[800],
+                                  size: 30,
+                                ),
+                                const SizedBox(height: 15.0),
+                                const Text(
+                                  "A post with an image looks more attractive",
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                const SizedBox(height: 15.0),
+                                Text(
+                                  "Browse",
+                                  style: TextStyle(
+                                    color: Colors.blue[800],
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          //Text(widget.post.description),
-                          const SizedBox(height: 10.0),
-
-                          Center(
-                            child: FutureBuilder<void>(
-                              future: retriveLostData(),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<void> snapshot) {
-                                switch (snapshot.connectionState) {
-                                  case ConnectionState.none:
-                                  case ConnectionState.waiting:
-                                    return const Text('Picked an image');
-                                  case ConnectionState.done:
-                                    return _previewImage();
-                                  default:
-                                    return const Text('Picked an image');
-                                }
-                              },
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  )
+                :const SizedBox(),
+          ],
         ),
 
         floatingActionButton: FloatingActionButton(
