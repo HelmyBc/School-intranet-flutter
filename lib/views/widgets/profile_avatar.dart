49af786bb98a4230c724507d1e6c5715bcf2a1a3
HelmyBc <helmibouchiha@gmail.com>
2022-05-15 14:52:20 +0100

    version 10.5 users' profile images are now shimmed

--- lib/views/widgets/profile_avatar.dart
+++ lib/views/widgets/profile_avatar.dart
@@ -1,5 +1,6 @@
 import 'package:avatar_view/avatar_view.dart';
 import 'package:enetcom_app/config/palette.dart';
+import 'package:enetcom_app/views/widgets/shimmer_widget.dart';
 
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class ProfileAvatar extends StatelessWidget {
 
   const ProfileAvatar({
     Key? key,
-    required this.imageUrl,
+    this.imageUrl = "assets/images/default_profile_image.png",
     this.isActive = false,
     this.hasBorder = false,
     this.raduis = 20.0,
@@ -19,45 +20,43 @@ class ProfileAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Stack(
-        children: [
-          AvatarView(
-            radius: raduis,
-            borderColor: Colors.grey,
-            avatarType: AvatarType.CIRCLE,
-            backgroundColor: Colors.red,
-            imagePath: imageUrl,
-            placeHolder: const Icon(
-              Icons.person,
-              size: 18,
-            ),
-            errorWidget: const Icon(
-              Icons.error,
-              size: 18,
-            ),
+    return Stack(
+      children: [
+        AvatarView(
+          radius: raduis,
+          borderColor: Colors.grey,
+          avatarType: AvatarType.CIRCLE,
+          backgroundColor: Colors.red,
+          imagePath: imageUrl,
+          placeHolder: ShimmerWidget.rectangular(
+            width: raduis * 2,
+            height: raduis * 2,
+            raduis: raduis,
           ),
-          isActive
-              ? Positioned(
-                  bottom: 0.0,
-                  right: 0.0,
-                  child: Container(
-                    height: 15.0,
-                    width: 15.0,
-                    decoration: BoxDecoration(
-                      color: Palette.online,
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 2.0,
-                        color: Colors.white,
-                      ),
+          errorWidget: const Icon(
+            Icons.person,
+            size: 18,
+          ),
+        ),
+        isActive
+            ? Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  height: 15.0,
+                  width: 15.0,
+                  decoration: BoxDecoration(
+                    color: Palette.online,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 2.0,
+                      color: Colors.white,
                     ),
                   ),
-                )
-              : const SizedBox.shrink(),
-        ],
-      ),
+                ),
+              )
+            : const SizedBox.shrink(),
+      ],
     );
   }
 }
